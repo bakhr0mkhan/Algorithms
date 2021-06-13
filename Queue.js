2bbddc731870adb07c3f947300cd8e7f7f2c7013
Bakhromkhan <67605887+bakhr0mkhan@users.noreply.github.com>
2021-06-13 21:20:34 +0500

    done queues and priority queues

---
+++ Queue.js
@@ -0,0 +1,35 @@
+function Queue() {
+  var collection = [];
+  // print to get it printed
+  this.print = function () {
+    return collection;
+  };
+  // enqueue to push element
+  this.enqueue = function (element) {
+    return collection.push(element);
+  };
+  // dequeue to remove element from the collection
+  this.dequeue = function () {
+    return collection.shift();
+  };
+  // front to get the first element
+  this.front = function () {
+    return collection[0];
+  };
+  // size
+  this.size = function () {
+    return collection.size;
+  };
+  // check if it is empty
+  this.isEmpty = function () {
+    return collection.length === 0;
+  };
+}
+var q = new Queue();
+q.enqueue("a");
+q.enqueue("b");
+q.enqueue("c");
+console.log(q.print());
+q.dequeue();
+console.log(q.front());
+console.log(q.print());
